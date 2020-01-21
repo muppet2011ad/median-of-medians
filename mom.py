bd4011cd10ce785980d2b677c90c6322618974ae
James Townsend <j.a.townsend2000@gmail.com>
2020-01-21 11:27:52 +0000

    Now plots the thing over time

--- mom.py
+++ mom.py
@@ -1,13 +1,14 @@
-import random
+import random, datetime
+import matplotlib.pyplot as plt
 
-def median_of_medians(A, i):
+def median_of_medians(A, i, gsize = 5):
     # Base case
-    if len(A) <= 5:
+    if len(A) <= gsize:
         return sorted(A)[i]
     # Find pivot
-    groups = [A[5*x:5*(x+1)] for x in range(len(A)//5)]
-    if len(A) % 5 != 0:
-        groups.append(A[5*len(groups):])
+    groups = [A[gsize*x:gsize*(x+1)] for x in range(len(A)//gsize)]
+    if len(A) % gsize != 0:
+        groups.append(A[gsize*len(groups):])
     medians = [sorted(group)[len(group)//2] for group in groups]
     final_mom = median_of_medians(medians, len(medians)//2)
     # Recurse
@@ -21,14 +22,23 @@ def median_of_medians(A, i):
     else:
         return median_of_medians(p1[:-1],i)
 
-median_of_medians([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],3)
+times = []
+for g in range(1,99,2):
+    start = datetime.datetime.now()
+    for i in range(100):
+        A = list(range(50000))
+        random.shuffle(A)
+        i = random.randint(0, 50000-1)
+        x = median_of_medians(A,i,g)
+        if x == i:
+            pass
+        else:
+            print("Something has gone wrong.")
+    end = datetime.datetime.now()
+    elapsed = end - start
+    times.append(elapsed)
 
-for i in range(10):
-    A = list(range(50000))
-    random.shuffle(A)
-    i = random.randint(0, 50000-1)
-    x = median_of_medians(A,i)
-    if x == i:
-        print("OK")
-    else:
-        print("Something has gone wrong.")
+xdata = [2*x + 1 for x in range(len(times))]
+ydata = [y.seconds + y.microseconds/1E6 for y in times]
+plt.plot(xdata,ydata)
+plt.show()
